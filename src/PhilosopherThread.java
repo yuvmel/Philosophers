dbbbaabeec56357e37f4da00f9f6320fdabe70a3
yuval.melamed <g077aGit>
2017-06-24 11:43:53 +0300

    ph1

---
+++ src/PhilosopherThread.java
@@ -0,0 +1,15 @@
+/*
+ * Maman 15 course 20554 question 2 by Yuval Melamed, ID 035870864
+ */
+
+/**
+ *
+ * @author yuval.melamed
+ */
+public class PhilosopherThread implements Runnable {
+
+    private static final int MAX_MSEC_FOR_ACTION = 1000;
+    private final boolean done = false;
+    private static final int id;
+    
+}
